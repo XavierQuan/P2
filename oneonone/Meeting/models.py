27522c1ffca539f09455b66d9b50cd9806aed01a
Kunping_Quan <xavvvierquan@outlook.com>
2024-03-03 22:19:19 -0500

    f

--- oneonone/Meeting/models.py
+++ oneonone/Meeting/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from ..Account.models import User
 
-# Create your models here.
+
+class PendingMeeting(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_limit = models.DateTimeField()
+    message = models.TextField()
+    title = models.CharField(max_length=255)
+    deadline = models.DateTimeField()
+
+
+class TimeSlot(models.Model):
+    meeting = models.ForeignKey(PendingMeeting, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    priority = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Participant(models.Model):
+    meeting = models.ForeignKey(PendingMeeting, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    response = models.BooleanField()
+    content = models.TextField()
+
+
+class FinalizedMeeting(models.Model):
+    meeting = models.OneToOneField(PendingMeeting, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    time = models.DateTimeField()
+    time_limit = models.DateTimeField()
+    participants = models.ManyToManyField(User)
+
+
+class SuggestedSchedule(models.Model):
+    meeting = models.ForeignKey(PendingMeeting, on_delete=models.CASCADE)
+
+
+class SuggestedTimeSlot(models.Model):
+    meeting = models.ForeignKey(SuggestedSchedule, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    duration = models.DurationField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
